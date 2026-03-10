8303ed2c5c9a378159473dfa4bd94e58a3b10998
amirzenoozi <amirzenoozi72@gmail.com>
2026-03-10 10:29:54 +0100

    :sparkles: Feature: Add utility function to convert time format to seconds

--- scripts/utils.py
+++ scripts/utils.py
@@ -38,3 +38,10 @@ def convert_mb_to_bytes(mb: float) -> int:
         return int(float(mb) * 1024 * 1024)
     except (ValueError, TypeError):
         return 0
+
+
+def to_seconds(t):
+    parts = list(map(int, t.split(':')))
+    if len(parts) == 3: return parts[0] * 3600 + parts[1] * 60 + parts[2]
+    if len(parts) == 2: return parts[0] * 60 + parts[1]
+    return parts[0]
